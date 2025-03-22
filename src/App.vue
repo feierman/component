1f08303fe306693c3976125e6990771ebc950c05
feierman <1981879111@qq.com>
2025-03-22 23:15:51 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+<DefaultLayout />
+</template>
+
+<script setup lang="ts">
+import DefaultLayout from './layouts/defaultLayout/Default.vue'
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/layouts/defaultLayout/Default.vue
@@ -0,0 +1,5 @@
+<template>
+<h2>默认布局</h2>
+</template>
+<script lang="ts" setup>
+</script>
